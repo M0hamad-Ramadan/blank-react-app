e4324a38d21df230ce8640fe70ce4fb0e4e3d34f
mohammed <moaboelyazied@gmail.com>
2024-03-12 13:44:04 +0200

    'read-write-useState'

--- src/MyBtn.js
+++ src/MyBtn.js
@@ -1,19 +1,16 @@
 import { useState } from "react";
 
 export default function MyBtn() {
-  console.log('render');
-  const state = useState("mohammed");
-  const value = state[0];
-  const setValue = state[1];
-
- function btnClicked() {
-    setValue('omar')
+  const [name, setName] = useState("mohammed");
+  
+  function changeName() {
+    name === "mohammed" ? setName("omar") : setName("mohammed");
   }
 
   return (
     <>
-      <button onClick={btnClicked}>change name</button>
-      <h1>{value}</h1>
+      <button onClick={changeName}>change name</button>
+      <h1>{name}</h1>
     </>
   );
 }
