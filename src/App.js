d597cb741f834d8c03625ed5d962f04119b90ef1
mohammed <moaboelyazied@gmail.com>
2024-03-12 14:26:06 +0200

    why-manageInpState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import MyBtn from "./MyBtn";
+import MyInpt from "./MyInpt";
 function App() {
-  return <div className="App"> <MyBtn /> </div>;
+  return <div className="App"> <MyBtn /> <MyInpt /> </div>;
 }
 
 // condition rendering
