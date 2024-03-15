341ec6dde91f4d5ccebc65b39bf79bfa6c200f06
mohammed <moaboelyazied@gmail.com>
2024-03-15 23:22:31 +0200

    mange-text-area-state

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,33 +1,49 @@
 import { useState } from "react";
 
-export default function MyForm() {
-  const [formInps, setFormInps] = useState({ userName: "", userEmail: "" });
-
+const MyForm = () => {
+  const [myFormData, setNewState] = useState({ name: "", email: "", peo: "" });
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        console.log(myFormData);
       }}>
-      <input
-        type="text"
-        placeholder="name..."
-        value={formInps.userName}
-        onChange={(e) => {
-          setFormInps({ ...formInps, userName: e.target.value });
-        }}
-      />
+      <div>
+        <label>name:</label>
+        <input
+          type="text"
+          value={myFormData.name}
+          onChange={(e) => {
+            setNewState({ ...myFormData, name: e.target.value });
+          }}
+        />
+      </div>
 
+      <div>
+        <label>email:</label>
+        <input
+          type="text"
+          value={myFormData.email}
+          onChange={(e) => {
+            setNewState({ ...myFormData, email: e.target.value });
+          }}
+        />
+      </div>
 
-      <input
-        type="text"
-        placeholder="email...."
-        value={formInps.userEmail}
-        onChange={(e) => {
-          setFormInps({ ...formInps, userEmail: e.target.value });
-        }}
-      />
+      <div>
+        <label>Peo:</label>
+        <textarea
+          type="text"
+          value={myFormData.peo}
+          onChange={(e) => {
+            setNewState({ ...myFormData, peo: e.target.value });
+          }}
+        />
+      </div>
 
       <button>Submit</button>
     </form>
   );
-}
+};
+
+export default MyForm;
