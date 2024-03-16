177aa1c9301846b8607fa395784d3cd7f5eb0a29
mohammed <moaboelyazied@gmail.com>
2024-03-16 02:09:39 +0200

    radio-and-state-2

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,14 +1,7 @@
 import { useState } from "react";
 
 const MyForm = () => {
-  const [myFormData, setNewState] = useState({
-    name: "",
-    email: "",
-    peo: "",
-    male: false,
-    fmale: false,
-    lang: "",
-  });
+  const [myFormData, setNewState] = useState({ status: "" });
   return (
     <form
       onSubmit={(e) => {
@@ -16,93 +9,43 @@ const MyForm = () => {
         console.log(myFormData);
       }}>
       <div className="inp-group">
-        <label>name:</label>
-        <input
-          type="text"
-          defaultValue={myFormData.name}
-          onChange={(e) => {
-            setNewState({ ...myFormData, name: e.target.value });
-          }}
-        />
-      </div>
-
-      <div className="inp-group">
-        <label>email:</label>
-        <input
-          type="text"
-          defaultValue={myFormData.email}
-          onChange={(e) => {
-            setNewState({ ...myFormData, email: e.target.value });
-          }}
-        />
-      </div>
-
-      <div className="inp-group">
-        <label>Peo:</label>
-        <textarea
-          type="text"
-          defaultValue={myFormData.peo}
-          onChange={(e) => {
-            setNewState({ ...myFormData, peo: e.target.value });
-          }}
-        />
-      </div>
-
-      <div className="inp-group">
-        <label>male:</label>
-        <input
-          type="checkbox"
-          checked={myFormData.male}
-          onChange={(e) => {
-            setNewState({ ...myFormData, male: e.target.checked });
-          }}
-        />
-      </div>
-
-      <div className="inp-group">
-        <label>fmale:</label>
-        <input
-          type="checkbox"
-          checked={myFormData.fmale}
-          onChange={(e) => {
-            setNewState({ ...myFormData, fmale: e.target.checked });
-          }}
-        />
-      </div>
-
-      <div className="inp-group">
         <p>Please select Web language:</p>
         <input
           type="radio"
-          name="fav_language"
+          name="status"
           value="HTML"
+          checked={myFormData.status === "HTML"}
           onChange={(e) => {
-            setNewState({ ...myFormData, lang: e.target.value });
+            setNewState({ ...myFormData, status: e.target.value });
           }}
         />
         &nbsp; <label htmlFor="html">HTML</label>
         <br />
         <input
           type="radio"
-          name="fav_language"
+          name="status"
           value="CSS"
+          checked={myFormData.status === "CSS"}
           onChange={(e) => {
-            setNewState({ ...myFormData, lang: e.target.value });
+            setNewState({ ...myFormData, status: e.target.value });
           }}
         />
         &nbsp; <label htmlFor="css">CSS</label>
         <br />
         <input
           type="radio"
-          name="fav_language"
+          name="status"
           value="JavaScript"
+          checked={myFormData.status === "JavaScript"}
           onChange={(e) => {
-            setNewState({ ...myFormData, lang: e.target.value });
+            setNewState({ ...myFormData, status: e.target.value });
           }}
         />
         &nbsp; <label htmlFor="javascript">JavaScript</label>
       </div>
 
+      <br></br>
+      <br></br>
       <button>Submit</button>
     </form>
   );
